f4475383c22511d9682bf09cc8b7257e36bcaef8
Wes Beard <wesley.beard@mymail.champlain.edu>
2021-12-07 14:15:44 -0500

    Worked on layouts and modeled JSON

--- app/src/main/java/com/example/chitchat/MainActivity.kt
+++ app/src/main/java/com/example/chitchat/MainActivity.kt
@@ -1,7 +1,17 @@
 package com.example.chitchat
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
 import okhttp3.*
 import kotlin.concurrent.thread
 
@@ -11,13 +21,81 @@ const val CLIENT = "wesley.beard@mymail.champlain.edu"
 var client: OkHttpClient = OkHttpClient()
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var messagesRecycler: RecyclerView
+    private lateinit var sendButton: ImageButton
+    private lateinit var messageBox: EditText
+
+    lateinit var messages: List<Message>
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        title = "ChitChat"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        thread {
-            var messages = getMessages(limit = 100)
-            print(messages)
+        val loadingThread = Thread {
+            var rawMessages = getMessages(limit = 3)
+            val gson = Gson()
+            val deserializedMessages = gson.fromJson(rawMessages, Messages::class.java)
+
+            messages = deserializedMessages.messages
+        }
+
+        loadingThread.start() // spawn thread
+        loadingThread.join() // wait for thread to finish
+
+        messagesRecycler = findViewById(R.id.messages_recycler)
+        messagesRecycler.layoutManager = GridLayoutManager(this@MainActivity,1)
+        messagesRecycler.adapter = RecyclerAdapter()
+    }
+
+    inner class MessageViewHolder(messageView: View) : RecyclerView.ViewHolder(messageView) {
+        lateinit var message: Message
+        private val messageView: TextView = messageView.findViewById(R.id.message_display)
+
+        init {
+            message = messages[position]
+            this.messageView.text = message.message
+        }
+
+        fun initIndex(position: Int) {
+            message = messages[position]
+        }
+    }
+
+    inner class RecyclerAdapter : RecyclerView.Adapter<MessageViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+            val inflater: LayoutInflater = LayoutInflater.from(parent.context)
+            val cellView: View = inflater.inflate(R.layout.message, parent, false)
+            return MessageViewHolder(cellView)
+        }
+
+        override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+            holder.initIndex(position)
+
+            // Operations to perform once the last cell is created
+//            if (position == grid.totalCells - 1) {
+//
+//                // Set grid to intent extra if it exists
+//                if (intent.hasExtra("grid")) {
+//                    val json = intent.getStringExtra("grid")
+//                    if (json != null) {
+//                        grid.setFromJson(json, this@MainActivity)
+//                    }
+//                }
+//
+//                // Separate thread for calculating neighbors of each cell
+//                thread {
+//                    for (cell in grid.cells.values) {
+//                        cell.calculateNeighbors(grid)
+//                    }
+//                }
+//            }
+        }
+
+        override fun getItemCount(): Int {
+            return 1
         }
     }
 }
---
+++ app/src/main/java/com/example/chitchat/Message.kt
@@ -0,0 +1,25 @@
+package com.example.chitchat
+
+import com.google.gson.annotations.SerializedName
+
+data class Messages(
+    @SerializedName("count") var id: Int,
+    @SerializedName("messages") var messages: List<Message>
+)
+
+// {"_id":"61afa37a7172481c519d8bf3","client":"jacob.capra@mymail.champlain.edu",
+// "date":"Tue, 07 Dec 2021 18:10:02 GMT","dislikes":1,"ip":"184.171.151.148",
+// "likes":0,"loc":[null,null],"message":"another test"}
+data class Message(
+    @SerializedName("_id") var id: String,
+    @SerializedName("client") var client: String,
+    @SerializedName("date") var date: String,
+    @SerializedName("dislikes") var dislikes: Int,
+    @SerializedName("ip") var ip: String,
+    @SerializedName("likes") var likes: Int,
+    // @SerializedName("loc") var loc: String,
+    @SerializedName("message") var message: String
+)
+
+
+
